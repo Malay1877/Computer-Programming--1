975bdb88716335c0d6a6e2fd59c5755021f87ec2
Malay1877 <malaytwovaidya@gmail.com>
2025-10-19 20:46:08 +0530

    Add files via upload

---
+++ MatrixMulti2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void main(){
+    int A[3][3],B[3][3],C[3][3],i,j,k;
+    printf("Enter the first matrix\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d" , &A[i][j]);
+        }
+    }
+    printf("Enter the second Matrix\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d" , &B[i][j]);
+        }
+    }
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            C[i][j]=0;
+            for(k=0;k<3;k++){
+                C[i][j]+= A[i][k]*B[k][j];
+            }
+        }
+    }
+    printf("The resultant Matrix is:\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d " , C[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
