253b9c0ed7cb416a8c60d0a20dee9f1055acda3b
Malay1877 <malaytwovaidya@gmail.com>
2026-01-11 00:24:34 +0530

    Add files via upload

---
+++ hrs_min.py
@@ -0,0 +1,3 @@
+a=int(input("Enter the number of hours\n"))
+b=a*60
+print("The number of minutes are:" , b)
---
+++ rupees_dollars.py
@@ -0,0 +1,3 @@
+a=int(input("Enter the amount of rupees\n"))
+b=a/89.89
+print("The amount in dollars is:" , b)
