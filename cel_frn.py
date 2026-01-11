9e1c7362493a65962ff671f8a2219d33b7e2d812
Malay1877 <malaytwovaidya@gmail.com>
2026-01-11 14:54:31 +0530

    Add files via upload

---
+++ cel_frn.py
@@ -0,0 +1,3 @@
+a=int(input("Enter the temperature in degree celcius\n"))
+f=(9*a/5)+32
+print("The temperature in Fahreniet is:" , f)
---
+++ sim_int.py
@@ -0,0 +1,7 @@
+# Calculate simple interest.
+
+a=int(input("Enter the principle amount\n"))
+b=int(input("Enter the rate of interest in %\n"))
+c=int(input("Enter the time period\n"))
+si=(a*b*c)/100
+print("The simple interest is:" , si)
